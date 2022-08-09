8eed07908b6db04de29111d2f3526f782095438e
Sunit Corneleous <sunit15-10591@diu.edu.bd>
2022-08-09 23:11:27 +0600

    string concatenation problem added

---
+++ 3.concatenation.js
@@ -0,0 +1,10 @@
+/* 3) John’s teacher gave him two variables. Each variable contains a string.
+John’s teacher asked him to combine these two strings(‘I am going to
+be’ and ‘an awesome web developer’) and print them in one line. Help
+John write the program. */
+
+const stringOne = "I am going to be";
+const stringTwo = "an awesome web developer";
+
+const combined = stringOne.concat(stringTwo);
+console.log(combined);
