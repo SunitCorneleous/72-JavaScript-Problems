5e1075c1f2f8a779154f875961769e51dfb347dc
Sunit Corneleous <sunit15-10591@diu.edu.bd>
2022-08-10 00:01:24 +0600

    remainder problem added

---
+++ 4.remainder.js
@@ -0,0 +1,10 @@
+/* 4) Sarah’s mother is teaching her mathematics. She gave Sarah the number
+119 and asked her what the remainder would be if she divided the number
+by 5. Help Sarah write the program.
+ */
+
+let numberGiven = 119;
+
+let remainder = numberGiven % 5;
+
+console.log(remainder);
